b895295b81972aa4c06e4e56f82feb897a0d2966
IsmailCuruk <ismailcrk@gmail.com>
2019-04-28 00:38:09 +0200

    Added the hero object

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,11 @@
 // Write your JS here
+let hero = {
+    name: 'ibnu Çürük',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'airsoft gun',
+        damage: 2,
+    }
+}
