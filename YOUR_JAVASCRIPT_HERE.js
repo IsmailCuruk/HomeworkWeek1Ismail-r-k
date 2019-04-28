702f1fba475a1a78562bd2536d56432fe67e0db6
IsmailCuruk <ismailcrk@gmail.com>
2019-04-28 20:47:56 +0200

    Added an enemy and the function that defeats him. Added a form and a button that gives situation report (although both are not working/doing something interactive?

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,15 +1,17 @@
 // Write your JS here
+// SECTION 1
 let hero = {
     name: 'ibnu Çürük',
     heroic: true,
     inventory: [],
     health: 10,
     weapon: {
-        type: 'bare fists',
+        type: '',
         damage: 2,
     }
 };
 
+// SECTION 2
 function rest(someObject) {
     if (someObject.health === 10){
         alert("No need to take a rest; the hero's health is full.")
@@ -30,15 +32,41 @@ function pickUpItem(heroLike, weaponLike) {
 
 function equipWeapon(heroLike){
     if (heroLike.inventory.length === 0 ) {
-       alert("You have no weapon to equip; Find one!")
+       alert("You have no weapon to equip; Find one! Untill then, use your fists.")
     } else {
     heroLike.weapon = heroLike.inventory[0];
     alert(`You switched your weapon to ${heroLike.weapon.type}!`)
     }}
 
-    function displayStats(heroLike){
-        console.log("Hero: " + heroLike.name);
-        console.log("Health: " + heroLike.health);
-        console.log("Weapon: " + JSON.stringify(heroLike.weapon));
+function displayStats(heroLike){
+        //let showStatsInterface = document.getElementById('thisShowsStats').;
+        //function logger(heroLike){
+        (console.log(JSON.parse(JSON.stringify(heroLike))));
+        }    
+    //};
+// I CAN SEEM TO LOG THE STATUS TO THE CONSOLE, BUT I CAN'T GET IT DISPLAYED ON THE PAGE
+
+  
+function showInputAsStats(){ 
+    let showStatsInterface = document.getElementById('thisShowsStats');
+    showStatsInterface.innerHTML = displayStats(hero);
+    const p = document.createElement('p')
     }
- displayStats(hero)
+/*function changeName (){
+    const inputField = document.getElementById('userInputName');
+    const newName = inputField.value;
+    console.log(inputField);
+    }*/
+ 
+const enemy = {
+    health: 15,
+};
+
+function killEnemy(ourHero){
+   enemy.health = enemy.health - ourHero.weapon.damage;
+    if (enemy.health <=0 ){
+        alert("The enemy is dead")
+    } else {
+        alert("The bastard is still alive! You need to keep attacking.");
+    }
+}
